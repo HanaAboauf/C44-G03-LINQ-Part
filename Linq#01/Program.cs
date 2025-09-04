e8acf5e15c5ba97cdbdb4671e7f816b6d0e9049d
Hana <hanamarwasadek2004@gmail.com>
2025-09-04 04:53:03 +0300

    solution of question01

---
+++ Linq#01/Program.cs
@@ -0,0 +1,23 @@
+﻿using static Linq_01.Data.ListGenerator;
+namespace Linq_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Restriction Operators
+
+            #region Question01
+
+            //var results= ProductList?.Where(p => p.UnitsInStock == 0);
+
+            //foreach (var Item in results ?? Enumerable.Empty<object>())
+            //    Console.WriteLine(Item);
+
+            #endregion
+
+
+                #endregion
+        }
+    }
+}
