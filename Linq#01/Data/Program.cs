24a96dfa14ad344facda79e88d68bc5806897ec8
Hana <hanamarwasadek2004@gmail.com>
2025-09-04 04:31:12 +0300

    Add project files.

---
+++ Linq#01/Data/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq_01.Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
