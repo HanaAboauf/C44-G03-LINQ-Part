4b95ebf5e08dedbe71fc5e6943ea251f0c5ba027
Hana <hanamarwasadek2004@gmail.com>
2025-09-07 21:14:43 +0300

    solution of question01 part01

---
+++ Linq#02/Program.cs
@@ -0,0 +1,28 @@
+﻿using static Linq_02.Data.ListGenerator;
+using static Linq_02.Data.Order;
+
+namespace Linq_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Element Operators
+
+            #region Question01
+
+            //1. Get first Product out of Stock 
+
+            //var result = ProductList?.First(p=>p.UnitsInStock==0);
+
+            //Console.WriteLine(result);
+
+
+
+            
+            #endregion
+
+            #endregion
+        }
+    }
+}
